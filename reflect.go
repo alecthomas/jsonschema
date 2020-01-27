42cb9b014a9687f68669cb5071d1bb48f81b681a
Matt Mc <4984708+tooolbox@users.noreply.github.com>
2020-01-26 16:55:49 -0800

    Use OrderedMap to preserve sequence of properties. (#51)

--- reflect.go
+++ reflect.go
@@ -14,6 +14,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/iancoleman/orderedmap"
 )
 
 // Version is the JSON Schema version.
@@ -34,33 +36,33 @@ type Type struct {
 	Version string `json:"$schema,omitempty"` // section 6.1
 	Ref     string `json:"$ref,omitempty"`    // section 7
 	// RFC draft-wright-json-schema-validation-00, section 5
-	MultipleOf           int              `json:"multipleOf,omitempty"`           // section 5.1
-	Maximum              int              `json:"maximum,omitempty"`              // section 5.2
-	ExclusiveMaximum     bool             `json:"exclusiveMaximum,omitempty"`     // section 5.3
-	Minimum              int              `json:"minimum,omitempty"`              // section 5.4
-	ExclusiveMinimum     bool             `json:"exclusiveMinimum,omitempty"`     // section 5.5
-	MaxLength            int              `json:"maxLength,omitempty"`            // section 5.6
-	MinLength            int              `json:"minLength,omitempty"`            // section 5.7
-	Pattern              string           `json:"pattern,omitempty"`              // section 5.8
-	AdditionalItems      *Type            `json:"additionalItems,omitempty"`      // section 5.9
-	Items                *Type            `json:"items,omitempty"`                // section 5.9
-	MaxItems             int              `json:"maxItems,omitempty"`             // section 5.10
-	MinItems             int              `json:"minItems,omitempty"`             // section 5.11
-	UniqueItems          bool             `json:"uniqueItems,omitempty"`          // section 5.12
-	MaxProperties        int              `json:"maxProperties,omitempty"`        // section 5.13
-	MinProperties        int              `json:"minProperties,omitempty"`        // section 5.14
-	Required             []string         `json:"required,omitempty"`             // section 5.15
-	Properties           map[string]*Type `json:"properties,omitempty"`           // section 5.16
-	PatternProperties    map[string]*Type `json:"patternProperties,omitempty"`    // section 5.17
-	AdditionalProperties json.RawMessage  `json:"additionalProperties,omitempty"` // section 5.18
-	Dependencies         map[string]*Type `json:"dependencies,omitempty"`         // section 5.19
-	Enum                 []interface{}    `json:"enum,omitempty"`                 // section 5.20
-	Type                 string           `json:"type,omitempty"`                 // section 5.21
-	AllOf                []*Type          `json:"allOf,omitempty"`                // section 5.22
-	AnyOf                []*Type          `json:"anyOf,omitempty"`                // section 5.23
-	OneOf                []*Type          `json:"oneOf,omitempty"`                // section 5.24
-	Not                  *Type            `json:"not,omitempty"`                  // section 5.25
-	Definitions          Definitions      `json:"definitions,omitempty"`          // section 5.26
+	MultipleOf           int                    `json:"multipleOf,omitempty"`           // section 5.1
+	Maximum              int                    `json:"maximum,omitempty"`              // section 5.2
+	ExclusiveMaximum     bool                   `json:"exclusiveMaximum,omitempty"`     // section 5.3
+	Minimum              int                    `json:"minimum,omitempty"`              // section 5.4
+	ExclusiveMinimum     bool                   `json:"exclusiveMinimum,omitempty"`     // section 5.5
+	MaxLength            int                    `json:"maxLength,omitempty"`            // section 5.6
+	MinLength            int                    `json:"minLength,omitempty"`            // section 5.7
+	Pattern              string                 `json:"pattern,omitempty"`              // section 5.8
+	AdditionalItems      *Type                  `json:"additionalItems,omitempty"`      // section 5.9
+	Items                *Type                  `json:"items,omitempty"`                // section 5.9
+	MaxItems             int                    `json:"maxItems,omitempty"`             // section 5.10
+	MinItems             int                    `json:"minItems,omitempty"`             // section 5.11
+	UniqueItems          bool                   `json:"uniqueItems,omitempty"`          // section 5.12
+	MaxProperties        int                    `json:"maxProperties,omitempty"`        // section 5.13
+	MinProperties        int                    `json:"minProperties,omitempty"`        // section 5.14
+	Required             []string               `json:"required,omitempty"`             // section 5.15
+	Properties           *orderedmap.OrderedMap `json:"properties,omitempty"`           // section 5.16
+	PatternProperties    map[string]*Type       `json:"patternProperties,omitempty"`    // section 5.17
+	AdditionalProperties json.RawMessage        `json:"additionalProperties,omitempty"` // section 5.18
+	Dependencies         map[string]*Type       `json:"dependencies,omitempty"`         // section 5.19
+	Enum                 []interface{}          `json:"enum,omitempty"`                 // section 5.20
+	Type                 string                 `json:"type,omitempty"`                 // section 5.21
+	AllOf                []*Type                `json:"allOf,omitempty"`                // section 5.22
+	AnyOf                []*Type                `json:"anyOf,omitempty"`                // section 5.23
+	OneOf                []*Type                `json:"oneOf,omitempty"`                // section 5.24
+	Not                  *Type                  `json:"not,omitempty"`                  // section 5.25
+	Definitions          Definitions            `json:"definitions,omitempty"`          // section 5.26
 	// RFC draft-wright-json-schema-validation-00, section 6, 7
 	Title       string        `json:"title,omitempty"`       // section 6.1
 	Description string        `json:"description,omitempty"` // section 6.1
@@ -121,7 +123,7 @@ func (r *Reflector) ReflectFromType(t reflect.Type) *Schema {
 		st := &Type{
 			Version:              Version,
 			Type:                 "object",
-			Properties:           map[string]*Type{},
+			Properties:           orderedmap.New(),
 			AdditionalProperties: []byte("false"),
 		}
 		if r.AllowAdditionalProperties {
@@ -263,7 +265,7 @@ func (r *Reflector) reflectStruct(definitions Definitions, t reflect.Type) *Type
 		if reflect.TypeOf(ignored) == t {
 			st := &Type{
 				Type:                 "object",
-				Properties:           map[string]*Type{},
+				Properties:           orderedmap.New(),
 				AdditionalProperties: []byte("true"),
 			}
 			definitions[t.Name()] = st
@@ -277,7 +279,7 @@ func (r *Reflector) reflectStruct(definitions Definitions, t reflect.Type) *Type
 	}
 	st := &Type{
 		Type:                 "object",
-		Properties:           map[string]*Type{},
+		Properties:           orderedmap.New(),
 		AdditionalProperties: []byte("false"),
 	}
 	if r.AllowAdditionalProperties {
@@ -314,7 +316,7 @@ func (r *Reflector) reflectStructFields(st *Type, definitions Definitions, t ref
 		property := r.reflectTypeToSchema(definitions, f.Type)
 		property.structKeywordsFromTags(f, st, name)
 
-		st.Properties[name] = property
+		st.Properties.Set(name, property)
 		if required {
 			st.Required = append(st.Required, name)
 		}
