8592d944db8f50feb662730fbdc0f704e09c93c3
Sam Lown <sam.lown@cabify.com>
2021-12-30 16:48:25 +0000

    Refactor comment extrator into own source file

--- reflect.go
+++ reflect.go
@@ -8,21 +8,12 @@ package jsonschema
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/fs"
 	"net"
 	"net/url"
-	gopath "path"
-	"path/filepath"
 	"reflect"
 	"strconv"
 	"strings"
 	"time"
-
-	"go/ast"
-	"go/doc"
-	"go/parser"
-	"go/token"
 
 	"github.com/iancoleman/orderedmap"
 )
@@ -894,82 +885,12 @@ func fullyQualifiedTypeName(t reflect.Type) string {
 	return t.PkgPath() + "." + t.Name()
 }
 
-// AddGoComments will read all the go files contained in the provided path,
-// including sub-directories, in order to generate a dictionary of comments
-// associated with Types and Fields. The results will be added to the `CommentsMap`
-// to be included in Schema "description" fields.
-//
-// The `go/parser` library is used to extract all the comments and unfortunately doesn't
-// have a built-in way to determine the fully qualified name of a package. The `base` paremeter,
-// the URL used to import that package, is thus required to be able to match reflected types.
-//
-// When parsing type comments, we use the `go/doc`'s Synopsis method to extract the first phrase
-// only. Field comments, which tend to be much short, will include everything.
+// AddGoComments will update the reflectors comment map with all the comments
+// found in the provided source directories. See the #ExtractGoComments method
+// for more details.
 func (r *Reflector) AddGoComments(base, path string) error {
 	if r.CommentMap == nil {
 		r.CommentMap = make(map[string]string)
 	}
-
-	fset := token.NewFileSet()
-	dict := make(map[string][]*ast.Package)
-	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			d, err := parser.ParseDir(fset, path, nil, parser.ParseComments)
-			if err != nil {
-				return err
-			}
-			for _, v := range d {
-				// paths may have multiple packages, like for tests
-				k := gopath.Join(base, path)
-				dict[k] = append(dict[k], v)
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	for pkg, p := range dict {
-		for _, f := range p {
-			gtxt := ""
-			typ := ""
-			ast.Inspect(f, func(n ast.Node) bool {
-				switch x := n.(type) {
-				case *ast.TypeSpec:
-					typ = x.Name.String()
-					if !ast.IsExported(typ) {
-						typ = ""
-					} else {
-						txt := x.Doc.Text()
-						if txt == "" && gtxt != "" {
-							txt = gtxt
-							gtxt = ""
-						}
-						txt = doc.Synopsis(txt)
-						r.CommentMap[fmt.Sprintf("%s.%s", pkg, typ)] = strings.TrimSpace(txt)
-					}
-				case *ast.Field:
-					txt := x.Doc.Text()
-					if typ != "" && txt != "" {
-						for _, n := range x.Names {
-							if ast.IsExported(n.String()) {
-								k := fmt.Sprintf("%s.%s.%s", pkg, typ, n)
-								r.CommentMap[k] = strings.TrimSpace(txt)
-							}
-						}
-					}
-				case *ast.GenDecl:
-					// remember for the next type
-					gtxt = x.Doc.Text()
-				}
-				return true
-			})
-		}
-	}
-
-	return nil
+	return ExtractGoComments(base, path, r.CommentMap)
 }
